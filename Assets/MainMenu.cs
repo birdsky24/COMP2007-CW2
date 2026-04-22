16baf4f3338556c28faef89305936be1e4e05315
OscarK <osckenn@gmail.com>
2026-04-22 17:59:28 +0100

    Using Main Menu made from CW1 as start point

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        StartCoroutine(LoadWithDelay());
+    }
+
+    private IEnumerator LoadWithDelay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene(1); // Optionally: SceneManager.GetActiveScene().buildIndex + 1. Will load the next scene.
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Doesn't work in the editor because it could close unity and lose unsaved work
+    }
+
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadWithDelay2());
+    }
+
+    private IEnumerator LoadWithDelay2()
+    {
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene(0); // Loads main menu scene
+    }
+}
